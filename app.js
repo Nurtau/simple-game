7da8b358a7343e3a504dcd377d5527210497b4f5
Nurtau <nurtaut@gmail.com>
2020-10-07 12:37:10 +0600

    some updates

--- app.js
+++ app.js
@@ -8,33 +8,86 @@ const box = document.querySelector('.box');
 let position = [10, 10];
 let boxSize = 50;
 
+
+//function of moves
+
+const goUp = () => {
+    document.querySelector(".w").addEventListener("transitioncancel", () => {
+        document.querySelector(".w").style.background = "wheat";
+    });
+    document.querySelector(".w").style.background = "hotpink";
+    document.querySelector(".w").ontransitionend = () => {
+        document.querySelector(".w").style.background = "wheat";
+    }
+    if (position[0] > 0) {
+        position[0] -= 10;
+        box.style.top = `${position[0]}px`;
+    }
+
+
+};
+
+const goDown = () => {
+    document.querySelector(".s").addEventListener("transitioncancel", () => {
+        document.querySelector(".s").style.background = "wheat";
+    });
+    document.querySelector(".s").style.background = "hotpink";
+    document.querySelector(".s").ontransitionend = () => {
+        document.querySelector(".s").style.background = "wheat";
+    }
+    if (position[0] < (600 - boxSize)) {
+        position[0] += 10;
+        box.style.top = `${position[0]}px`;
+    }
+};
+
+const goLeft = () => {
+    document.querySelector(".a").addEventListener("transitioncancel", () => {
+        document.querySelector(".a").style.background = "wheat";
+    });
+    document.querySelector(".a").style.background = "hotpink";
+    document.querySelector(".a").ontransitionend = () => {
+        document.querySelector(".a").style.background = "wheat";
+    };
+    if (position[1] > 0) {
+        position[1] -= 10;
+        box.style.left = `${position[1]}px`;
+    }
+};
+
+const goRight = () => {
+    document.querySelector(".d").addEventListener("transitioncancel", () => {
+        document.querySelector(".d").style.background = "wheat";
+    });
+    document.querySelector(".d").style.background = "hotpink";
+    document.querySelector(".d").ontransitionend = () => {
+        document.querySelector(".d").style.background = "wheat";
+    }
+    if (position[1] < (1200 - boxSize)) {
+        position[1] += 10;
+        box.style.left = `${position[1]}px`;
+    }
+};
+
 document.addEventListener("keypress", (e) => {
     if (e.code === moves.up) {
-        if (position[0] > 0) {
-            position[0] -= 10;
-            box.style.top = `${position[0]}px`;
-        }
+        goUp();
 
     } else if (e.code === moves.down) {
-        if (position[0] < (600 - boxSize)) {
-            position[0] += 10;
-            box.style.top = `${position[0]}px`;
-        }
+        goDown();
 
     } else if (e.code === moves.left) {
-        if (position[1] > 0) {
-            position[1] -= 10;
-            box.style.left = `${position[1]}px`;
-        }
+        goLeft();
 
     } else if (e.code === moves.right) {
-        if (position[1] < (1200 - boxSize)) {
-            position[1] += 10;
-            box.style.left = `${position[1]}px`;
-        }
-
+        goRight();
     }
 });
+
+document.querySelector(".w").addEventListener("click", goUp);
+document.querySelector(".s").addEventListener("click", goDown);
+document.querySelector(".d").addEventListener("click", goRight);
+document.querySelector(".a").addEventListener("click", goLeft);
 
 
 //initialisation of food and score label
